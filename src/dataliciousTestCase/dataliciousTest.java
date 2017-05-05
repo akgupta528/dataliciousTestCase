8c44face2760e18a22266eac78559cba5fc46389
Akanksha <akgupta528@gmail.com>
2017-05-06 00:18:48 +0530

    Commit

--- src/dataliciousTestCase/dataliciousTest.java
+++ src/dataliciousTestCase/dataliciousTest.java
@@ -14,14 +14,13 @@ import net.lightbody.bmp.BrowserMobProxyServer;
 import net.lightbody.bmp.client.ClientUtil;
 import net.lightbody.bmp.core.har.Har;
 import net.lightbody.bmp.proxy.CaptureType;
+import dataliciousTestCase.lib;
 
 @Parameters({"pathForHarLog"})
 public class dataliciousTest {
-	public static WebDriver driver;
-	public static  BrowserMobProxy proxy;
-	protected int indexForEntries;
-	protected int indexForQueryString;
-	protected String value="test";	
+	public WebDriver driver;
+	public BrowserMobProxy proxy;
+
 
   @BeforeClass
   @Parameters({"browser","pathForFirefoxDriver","pathForChromeDriver"})
@@ -43,35 +42,38 @@ public class dataliciousTest {
   @Test
   @Parameters({"SearchItem","pathForHarLog"})
 	public void datalicious_network_log_test(String SearchItem,String pathForHarLog) throws InterruptedException, Exception {
-	  proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT);
-	  // create a new HAR with the label "datalicious.com"
-	  proxy.newHar("datalicious.com");
-	  driver.get("https://www.google.co.in/");
-	  startupPageObjectRepository.searchbox(driver).sendKeys(SearchItem);
-	  Thread.sleep(3000);
-	  startupPageObjectRepository.search_button(driver).click();
-	  startupPageObjectRepository.firstLink(driver).click();
-	  Thread.sleep(10000);
-	  // get the HAR data
-	  Har har = proxy.getHar();
-	  int totalNumberofEntries= har.getLog().getEntries().size();
-	  for(int indexForEntries=0;indexForEntries<totalNumberofEntries;indexForEntries++){
-		  if(har.getLog().getEntries().get(indexForEntries).getRequest().getUrl().indexOf("optimahub") !=-1 || har.getLog().getEntries().get(indexForEntries).getRequest().getUrl().indexOf("collect") !=-1 ){
-			  System.out.println(har.getLog().getEntries().get(indexForEntries).getRequest().getUrl());
-			  int queryStringSize=har.getLog().getEntries().get(indexForEntries).getRequest().getQueryString().size();
-			  for(indexForQueryString=0;indexForQueryString<queryStringSize;indexForQueryString++){
-			   if(har.getLog().getEntries().get(indexForEntries).getRequest().getQueryString().get(indexForQueryString).getName().equalsIgnoreCase("dt") || har.getLog().getEntries().get(indexForEntries).getRequest().getQueryString().get(indexForQueryString).getName().equalsIgnoreCase("dp")){
-				 value=har.getLog().getEntries().get(indexForEntries).getRequest().getQueryString().get(indexForQueryString).getValue();
-				 System.out.println(value);
-			  }
-		  }
-		  }
+	  lib.openBrowser(proxy,driver,SearchItem);
+	  lib.captureNetworkLog(proxy);
+	  lib.writeFile(pathForHarLog);
+	  
+	  
+//	  proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT);
+//	  // create a new HAR with the label "datalicious.com"
+//	  proxy.newHar("datalicious.com");
+//	  driver.get("https://www.google.co.in/");
+//	  startupPageObjectRepository.searchbox(driver).sendKeys(SearchItem);
+//	  Thread.sleep(3000);
+//	  startupPageObjectRepository.search_button(driver).click();
+//	  startupPageObjectRepository.firstLink(driver).click();
+//	  Thread.sleep(10000);
+//	  // get the HAR data
+//	  Har har = proxy.getHar();
+//	  int totalNumberofEntries= har.getLog().getEntries().size();
+//	  for(int indexForEntries=0;indexForEntries<totalNumberofEntries;indexForEntries++){
+//		  if(har.getLog().getEntries().get(indexForEntries).getRequest().getUrl().indexOf("optimahub") !=-1 || har.getLog().getEntries().get(indexForEntries).getRequest().getUrl().indexOf("collect") !=-1 ){
+//			  System.out.println(har.getLog().getEntries().get(indexForEntries).getRequest().getUrl());
+//			  int queryStringSize=har.getLog().getEntries().get(indexForEntries).getRequest().getQueryString().size();
+//			  for(indexForQueryString=0;indexForQueryString<queryStringSize;indexForQueryString++){
+//			   if(har.getLog().getEntries().get(indexForEntries).getRequest().getQueryString().get(indexForQueryString).getName().equalsIgnoreCase("dt") || har.getLog().getEntries().get(indexForEntries).getRequest().getQueryString().get(indexForQueryString).getName().equalsIgnoreCase("dp")){
+//				 value=har.getLog().getEntries().get(indexForEntries).getRequest().getQueryString().get(indexForQueryString).getValue();
+//				 System.out.println(value);
+//			  }
+//		  }
+//		  }
 	
 	  
   }
-		File file = new File(pathForHarLog);
-		FileOutputStream fos = new FileOutputStream(pathForHarLog);
-		fos.write(value.getBytes());
+		
 }
-}
+
   
